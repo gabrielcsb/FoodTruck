915176ac8b2514a6a9f37af469db3a10db0ef507
gabrielcsb <gabriel.c.s.brasil@gmail.com>
2023-01-19 15:16:18 -0300

    gateway

---
+++ foodtruck/src/main/java/br/com/tech4me/foodtruck/FoodtruckApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.foodtruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtruckApplication.class, args);
+	}
+
+}
---
+++ foodtruck/src/test/java/br/com/tech4me/foodtruck/FoodtruckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.foodtruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtruckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
